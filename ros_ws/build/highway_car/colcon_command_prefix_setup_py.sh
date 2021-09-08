1afe02f47a9043b4efae3af6c223b57c6a0dd41b
Houman-HM <hm.houman@gmail.com>
2021-09-08 19:52:54 +0300

    updating

--- ros_ws/build/highway_car/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/lenovo/ros2_foxy/src/Batch-Opt-Highway-Driving/ros_ws/install/msgs_car/share/msgs_car/package.sh"
--- ros_ws/build/msgs_car/rosidl_generator_py/msgs_car/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from msgs_car.msg._controls import Controls  # noqa: F401
-from msgs_car.msg._states import States  # noqa: F401
